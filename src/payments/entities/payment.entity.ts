9b9d77f625ebf1d3689c0525c4311053fdcd9c5c
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-07-03 17:53:15 +0300

    added @entity

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,1 +1,23 @@
-export class Payment {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn()
+  payment_id: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  amount: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  currency: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
