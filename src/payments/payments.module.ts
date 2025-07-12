f0358ee52d5ce0d5c247d1496acb0047ad933fda
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-07-12 14:20:48 +0300

    added deliveries module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { Order } from 'src/orders/entities/order.entity';
 import { OrdersModule } from 'src/orders/orders.module';
+import { DeliveriesModule } from 'src/deliveries/deliveries.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payment, Order]),
     OrdersModule,
+    DeliveriesModule,
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
