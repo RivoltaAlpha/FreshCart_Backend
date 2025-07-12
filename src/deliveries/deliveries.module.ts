faa3c19e2122914fdc41f539b4a6c3ce85358eb4
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-07-12 14:20:20 +0300

    create deliveries module with imports and exports

---
+++ src/deliveries/deliveries.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { DeliveriesService } from './deliveries.service';
+import { DeliveriesController } from './deliveries.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Delivery } from './entities/delivery.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Delivery])],
+  controllers: [DeliveriesController],
+  providers: [DeliveriesService],
+  exports: [DeliveriesService],
+})
+export class DeliveriesModule {}
